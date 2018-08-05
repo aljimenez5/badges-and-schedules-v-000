9aff6bfeedfcd1d8093c8858c711b8bb631621e2
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-05 23:41:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,4 +9,5 @@ end
 def assign_rooms(speakers_list)
   room_assignments = []
   speakers_list.each_with_index {|name, room| room_assignments << "Hello, #{name}! You'll be assigned to room #{room+1}!"}
+  return room_assignments
 end
