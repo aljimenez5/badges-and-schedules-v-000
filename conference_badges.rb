9961b706febc4287ac26869cde165d07da0ef031
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-05 23:38:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_names)
+  array_names.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(speakers_list)
+  speakers_list.each_with_index {|name, room| return "Hello, #{name}! You'll be assigned to room #{room+1}!"}
+  end
+end
